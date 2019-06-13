6cb05027cb5f52aa95372b6113cdb28ad48bd917
azhar difa arnanda <aazhard8@gmail.com>
2019-06-13 11:53:17 +0700

    Frontend: add sign up page

--- nataweb/base/urls.py
+++ nataweb/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.test_base, name='test_base') 
+    path('', views.test_base, name='test_base'),
+    path('sign_up/', views.sign_up, name='sign_up')
 ]
